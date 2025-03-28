6de8d249aeb830be30a2377e6f5cf6db4ac9ee86
Muhammad Iman <iman@tessinternational.com>
2025-03-28 11:28:10 +0800

    fix:

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[name].[hash].[ext]'
-      }
+      test: /\.(woff|woff2)$/,
+      type: "asset/resource", // Ensures fonts are treated as static files
     });
     return config;
   },
